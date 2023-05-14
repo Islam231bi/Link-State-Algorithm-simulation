33dbb723c7a6841564d13e9f4f250b7f15660cf9
islam231bi <islambassuni@gmail.com>
2023-05-14 10:25:27 +0200

    Added own Dijkstra implementation

--- main.py
+++ main.py
@@ -7,6 +7,52 @@ no_edges = 0
 least_cost_paths = [[]]
 
 
+# Dijkstra function
+def dijkstra_path(graph, src, dst, weight):
+    # Create a dictionary to store the shortest distances
+    distances = {node: float('inf') for node in graph.nodes}
+    distances[src] = 0
+
+    # Create a dictionary to store the previous node in the shortest path
+    previous = {node: None for node in graph.nodes}
+
+    # Create a set to store visited nodes
+    visited = set()
+
+    # Dijkstra's algorithm
+    while len(visited) < len(graph.nodes):
+        # Find the node with the minimum distance
+        min_dist_node = None
+        for node in graph.nodes:
+            if node not in visited and (min_dist_node is None or distances[node] < distances[min_dist_node]):
+                min_dist_node = node
+
+        # If the destination node is reached, break the loop
+        if min_dist_node == dst:
+            break
+
+        # Add the current node to the visited set
+        visited.add(min_dist_node)
+
+        # Update the distances and previous nodes of neighboring nodes
+        for neighbor in graph.neighbors(min_dist_node):
+            if neighbor in visited:
+                continue
+            new_distance = distances[min_dist_node] + graph.edges[min_dist_node, neighbor][weight]
+            if new_distance < distances[neighbor]:
+                distances[neighbor] = new_distance
+                previous[neighbor] = min_dist_node
+
+    # Construct the shortest path from source to destination
+    path = []
+    current_node = dst
+    while current_node is not None:
+        path.insert(0, current_node)
+        current_node = previous[current_node]
+
+    return path
+
+
 # Parse data from the input file
 with open('input.txt') as f:
     lines = f.readlines()
@@ -26,7 +72,7 @@ for n in G.nodes:
     for node in G.nodes:
         l = []
         if node != n:
-            l = nx.dijkstra_path(G,n,node,weight='weight')
+            l = dijkstra_path(G,n,node,weight='weight')
             print("  " + node + "                "+ "("+str(n)+", " + str(l[1]) + ")")
             if node == 'u':
                 least_cost_paths.append(l)
